43267bac331ba8503860ae616323238ca27a9f2a
judeharding <jharding@genesisgroupusa.com>
2020-09-29 15:53:29 -0400

    working thru 222

--- lib/widgets/tasks_list.dart
+++ lib/widgets/tasks_list.dart
@@ -11,13 +11,12 @@ class TasksList extends StatelessWidget {
     return Consumer<TaskData>(builder: (context, taskData, child) {
       return ListView.builder(
         itemBuilder: (context, index) {
+          final task = taskData.tasks[index];
           return TaskTile(
-            taskTitle: taskData.tasks[index].name,
-            isChecked: taskData.tasks[index].isDone,
+            taskTitle: task.name,
+            isChecked: task.isDone,
             checkboxCallback: (checkboxState) {
-              // setState(() {
-              //   Provider.of<TaskData>(context)..tasks[index].toggleDone();
-              // });
+              taskData.updateTask(task);
             },
           );
         },
